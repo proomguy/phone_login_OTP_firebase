22117cfbc21b9130033fce59bb4a58b7636b1edb
proomguy <mattz100bc@gmail.com>
2023-02-27 17:07:27 +0300

    Finalizing

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,4 +1,6 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:phone_login/login_screen.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -10,6 +12,41 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: Text(
+          "Nyumbani",
+          style: TextStyle(color: Colors.white),
+        ),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Image.asset("images/welcome.jpg"),
+          Container(
+            margin: EdgeInsets.all(65.0),
+            width: double.infinity,
+            child: ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                primary: Colors.deepPurpleAccent
+              ),
+              onPressed: (){
+                FirebaseAuth.instance.signOut();
+                Navigator.of(context).push(MaterialPageRoute(builder: (c) => LoginScreen()));
+              },
+              child: Text(
+                'Log Out',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 18,
+                  fontWeight: FontWeight.bold
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
